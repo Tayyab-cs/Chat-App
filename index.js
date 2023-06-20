51a85d0ecff4ad5cfb2be089229c07ca1e669321
Muhammad-Tayyab <muhammadtayyab.cs@gmail.com>
2023-06-20 19:19:47 +0500

    feat: Added nickName feat & user Con/Discon log

--- index.js
+++ index.js
@@ -17,8 +17,20 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+let connectedUsers = new Map();
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  // Set nickname for the connected user
+  socket.on('setNickName', (nickname) => {
+    console.log(socket.id, nickname);
+    connectedUsers.set(socket.id, nickname);
+    socket.broadcast.emit('userNickNameSet', `${nickname} has joined the chat`);
+  });
+
+  // Broadcast a message when someone connects
+  socket.broadcast.emit('userConnected', 'A new user has been connected.');
 
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
@@ -27,6 +39,12 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+
+    // Delete the socket to the disconnected users set
+    connectedUsers.delete(socket);
+
+    // Broadcast a message when someone disconnects
+    socket.broadcast.emit('userDisconnected', `A user has been disconnected`);
   });
 });
 
